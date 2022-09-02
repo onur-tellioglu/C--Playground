5597b24e27477c193faffe2a6b75bb5286f2fd46
Onur Tellioğlu <94357256+onur-tellioglu@users.noreply.github.com>
2022-09-03 00:36:23 +0300

    First get method is done

---
+++ Superhero API/Superhero API/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superhero_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero 
+                {
+                    Id = 0,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Malibu"
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+}
